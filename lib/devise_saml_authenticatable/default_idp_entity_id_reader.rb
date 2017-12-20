0458997584c1f4a0efc36da6902c5271b534f796
Tony Bruess <tony@oc.tc>
2017-12-20 16:24:32 -0600

    Include settings in valid? (for encrypted responses)

--- lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
@@ -1,14 +1,18 @@
 module DeviseSamlAuthenticatable
   class DefaultIdpEntityIdReader
+    include DeviseSamlAuthenticatable::SamlConfig
+
     def self.entity_id(params)
       if params[:SAMLRequest]
         OneLogin::RubySaml::SloLogoutrequest.new(
           params[:SAMLRequest],
+          settings: new.saml_config(),
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuer
       elsif params[:SAMLResponse]
         OneLogin::RubySaml::Response.new(
           params[:SAMLResponse],
+          settings: new.saml_config(),
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuers.first
       end
